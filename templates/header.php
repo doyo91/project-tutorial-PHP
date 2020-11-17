e9c88d0d0c1259136ce31ae3c5f552cb8e77fad2
doyo91 <doyo_91@hotmail.com>
2020-11-17 13:23:01 +0100

    header php

--- templates/header.php
+++ templates/header.php
@@ -1,10 +1,19 @@
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ninja Pizza || PHP tutorial</title>
     <!-- Compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <!-- css custom -->
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body class="grey lighten-4">
+    <nav class="white z-depth-0">
+        <div class="container">
+            <a href="#" class="brand-logo brand-text">Ninja Pizza</a>
+            <ul id="nav-mobile" class="right hide-on-small-and-down">
+                <li><a href="#" class="btn brand z-depth-0">Add a Pizza</a></li>
+            </ul>
+        </div>
+    </nav>
 
