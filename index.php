2f6527bcd7642b5b6b9f6773b3f7c4e1d7ce6c92
doyo91 <doyo_91@hotmail.com>
2020-11-17 17:13:14 +0100

    connecting to a database

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php 
+    // MySQLi o PDO
+    // connect to database
+    $conn = mysqli_connect('localhost', 'sam', 'test1234', 'tutorial_php');
+
+    // check connection
+    if(!$conn) {
+        echo 'Connection error: ' . mysqli_connect_error();
+    }
 
 ?>
 
