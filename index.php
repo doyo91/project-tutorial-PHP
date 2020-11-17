2fc2170d294127011c456045b0c43746bb852c6a
doyo91 <doyo_91@hotmail.com>
2020-11-17 13:28:34 +0100

    footer template

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
 <html lang="en">
     <?php include('templates/header.php'); ?>
 
+    <?php include('templates/footer.php'); ?>
+
     
-</body>
+
 </html>
--- templates/footer.php
+++ templates/footer.php
@@ -1,6 +1,6 @@
-
-
-
-
-
-?>
+    <footer class="section">
+        <div class="center grey-text">
+            Copyright 2020 &copy;Ninja Pizzas
+        </div>
+    </footer>
+</body>
